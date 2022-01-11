7f6dedeb2e53eb08e120180d67a4ae4810f15809
lafiteee <lafiteee@163.com>
2022-01-11 17:09:20 +0800

    修改为10个类别，取消验证集，取消top5准确率

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,13 +1,16 @@
 import os
 import shutil
 import copy
+import random
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torch.utils.data as data
 
+RANDOM_SEED = 1994
+random.seed(RANDOM_SEED)
 
-def make_train_test(images_dir, train_dir, test_dir, flag=False, TRAIN_RATIO=0.8, VALID_RATIO = 0.9):
 
+def make_train_test(images_dir, train_dir, test_dir, flag=False, TRAIN_RATIO=0.8, VALID_RATIO=0.9):
     if flag:
         classes = os.listdir(images_dir)
 
@@ -15,6 +18,9 @@ def make_train_test(images_dir, train_dir, test_dir, flag=False, TRAIN_RATIO=0.8
 
             class_dir = os.path.join(images_dir, c)
             images = os.listdir(class_dir)
+
+            # 随机打乱图片顺序
+            random.shuffle(image)
 
             n_train = int(len(images) * TRAIN_RATIO)
 
@@ -62,16 +68,18 @@ def make_train_test(images_dir, train_dir, test_dir, flag=False, TRAIN_RATIO=0.8
     test_data = datasets.ImageFolder(root=test_dir,
                                      transform=test_transforms)
 
-    n_train_examples = int(len(train_data) * VALID_RATIO)
-    n_valid_examples = len(train_data) - n_train_examples
+    n_train_examples = int(len(train_data))
+    # n_train_examples = int(len(train_data) * VALID_RATIO)
+    # n_valid_examples = len(train_data) - n_train_examples
 
-    train_data, valid_data = data.random_split(train_data,
-                                               [n_train_examples, n_valid_examples])
+    # train_data, valid_data = data.random_split(train_data,
+    #                                            [n_train_examples, n_valid_examples])
 
-    valid_data = copy.deepcopy(valid_data)
-    valid_data.dataset.transform = test_transforms
+    # valid_data = copy.deepcopy(valid_data)
+    # valid_data.dataset.transform = test_transforms
     print(f'Number of training examples: {len(train_data)}')
-    print(f'Number of validation examples: {len(valid_data)}')
+    # print(f'Number of validation examples: {len(valid_data)}')
     print(f'Number of testing examples: {len(test_data)}')
 
-    return train_data, valid_data, test_data
+    return train_data, test_data
+    # return train_data, valid_data, test_data
